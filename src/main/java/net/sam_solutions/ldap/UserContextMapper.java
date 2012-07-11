40b830e74412b6421ede1ff7dd226b11248405fc
unknown <ssav@SC0496.sam-solutions.net>
2012-07-11 12:20:05 +0300

    Roles

--- src/main/java/net/sam_solutions/ldap/UserContextMapper.java
+++ src/main/java/net/sam_solutions/ldap/UserContextMapper.java
@@ -1,11 +1,10 @@
 package net.sam_solutions.ldap;
 
-import org.apache.log4j.Logger;
 import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.DirContextOperations;
+import org.springframework.ldap.core.DistinguishedName;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
@@ -22,33 +21,14 @@ import java.util.Collection;
 public class UserContextMapper implements UserDetailsContextMapper {
     @Override
     public UserDetails mapUserFromContext(DirContextOperations ctx, String username, Collection<? extends GrantedAuthority> authorities) {
-
-        final Logger log = Logger.getLogger(this.getClass());
-        log.error("----------------------------------------------------- " +
-                "----------------------------------------------------- " +
-                "----------------------------------------------------- " +
-                "----------------------------------------------------- " +
-                "----------------------------------------------------- " +
-                "----------------------------------------------------- --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------" +
-                "");
-        String cns = ctx.getStringAttribute("ou");
-        log.error(cns + "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-        log.error(ctx.getDn() + "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-        Collection<? extends GrantedAuthority> auths = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
-        for (GrantedAuthority auth : auths) {
-            log.error(auth.getAuthority());
-        }
-        log.error("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        log.error(authorities.isEmpty());
+        DistinguishedName distinguishedName = new DistinguishedName(ctx.getDn());
+        String ou = distinguishedName.getLdapRdn("ou").getValue();
         Collection<GrantedAuthority> mappedAuthorities = new ArrayList<GrantedAuthority>();
-        for (GrantedAuthority granted : authorities) {
-            if ("PEOPLE".equalsIgnoreCase(granted.getAuthority())) {
-                mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-            } else if ("ADMIN".equalsIgnoreCase(granted.getAuthority())) {
-                mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-            }
-        }
-        if (mappedAuthorities.isEmpty()) {
+        if ("PEOPLE".equalsIgnoreCase(ou)) {
+            mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        } else if ("ADMIN".equalsIgnoreCase(ou)) {
+            mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        } else {
             mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
         }
         return new User(username, "", true, true, true, true, mappedAuthorities);
